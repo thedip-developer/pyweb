3b033844b5d1c97b46ce4831220ad5111e2ca650
thedip-developer <thedip.developer@gmail.com>
2020-05-02 11:05:09 +0600

    Code been written to listening port 8800 and on root get it is showing a line.

--- index.py
+++ index.py
@@ -1,2 +1,16 @@
 import tornado.web
 import tornado.ioloop
+
+class basicRequestHandler(tornado.web.RedirectHandler):
+    def get(self):
+        self.write("Hello, This is a python command that is running from the backend.")
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/", basicRequestHandler)
+    ])
+
+    port = 8800
+    app.listen(port)
+    print(f"Application is ready and listening port {port}")
+    tornado.ioloop.IOLoop.current().start()
